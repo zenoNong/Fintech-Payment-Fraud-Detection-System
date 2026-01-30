90588093efbdde2268e7b3adec4d3c8d1c4ee0c5
ZenoNong <102167083+zenoNong@users.noreply.github.com>
2026-01-31 01:53:18 +0530

    changing codes

--- auth-service/src/main/java/com/fintech/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/fintech/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.fintech.auth.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
